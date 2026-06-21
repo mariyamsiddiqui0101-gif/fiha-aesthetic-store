d7f981b8628b766307410a46ea9fd830906ca1a5
mariyamsiddiqui0101-gif <mariyamsiddiqui0101@gmail.com>
2026-06-21 12:51:59 +0530

    Implement automatic slider for bestseller products
    Added a slider functionality to automatically move through bestseller product cards every 3 seconds.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Fiha Store Loaded");
+let currentSlide = 0;
+
+function moveSlider() {
+  const slider = document.querySelector('.bestseller-slider');
+  if (!slider) return;
+
+  const cards = document.querySelectorAll('.bestseller-slider .product-card');
+  currentSlide++;
+
+  if (currentSlide >= cards.length) {
+    currentSlide = 0;
+  }
+
+  slider.scrollTo({
+    left: cards[currentSlide].offsetLeft,
+    behavior: 'smooth'
+  });
+}
+
+setInterval(moveSlider, 3000);
